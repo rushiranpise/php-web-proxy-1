faf3752427aef582d8a5b99be4c9ac56d4deb77b
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2020-05-16 10:29:53 +0530

    Update language.php

--- language.php
+++ language.php
@@ -1,9 +1,9 @@
 <?php
-$lang["slogan"] = "Remove the borders";
+$lang["slogan"] = "Removing The Borders, Surf Safe Net.";
 $lang["wikipedia"] = "Wikipedia";
 $lang["back"] = "Back";
 $lang["go"] = "Go";
 $lang["home"] = "Home";
-$lang["tos"] = "Enter here your Terms of Service text";
+$lang["tos"] = "https://rushiphp.herokuapp.com/";
 $lang["agree"] = "By visiting this page you agree to the";
-$lang["tos_2"] = "terms of use";
+$lang["tos_2"] = "Terms and Conditions to use this website.";
